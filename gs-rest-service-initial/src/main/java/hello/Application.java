a490524a56a165db4ed2eec1c462bf0fafff0ffe
Justin Davis <justintdavis8@gmail.com>
2018-10-27 17:34:24 -0500

    Added code to the application.java file to run the application.

--- gs-rest-service-initial/src/main/java/hello/Application.java
+++ gs-rest-service-initial/src/main/java/hello/Application.java
@@ -1,5 +1,13 @@
 package hello;
 
-public class Application {
+//import all necessary tools
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+            // launch the application
+        SpringApplication.run(Application.class, args);
+    }
 }
