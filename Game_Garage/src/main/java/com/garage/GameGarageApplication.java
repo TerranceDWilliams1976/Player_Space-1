290fadea5d83b12942abc76fc746386211880052
Justin Davis <justintdavis8@gmail.com>
2018-11-26 08:55:01 -0600

    updated app.

---
+++ Game_Garage/src/main/java/com/garage/GameGarageApplication.java
@@ -0,0 +1,12 @@
+package com.garage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameGarageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameGarageApplication.class, args);
+	}
+}
