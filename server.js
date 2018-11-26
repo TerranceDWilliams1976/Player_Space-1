cc65e1924a14d9180ce167ae54b562baad5ff4b7
Justin Davis <justintdavis8@gmail.com>
2018-11-26 15:00:39 -0600

    update

--- server.js
+++ server.js
@@ -1,12 +1,29 @@
-const express = require('express');
-const compression = require('compression');
+// const express = require('express');
+// const compression = require('compression');
 
-const CONTEXT = '/angular-ngrx-material-starter';
-const PORT = 4000;
+// const CONTEXT = '/angular-ngrx-material-starter';
+// const PORT = 4000;
+
+// const app = express();
+
+// app.use(compression());
+// app.use(CONTEXT, express.static(__dirname + '/dist'));
+// app.use('/', express.static(__dirname + '/dist'));
+// app.listen(PORT, () => console.log(`App running on localhost:${PORT}/${CONTEXT}`));
+
+//Install express server
+const express = require('express');
+const path = require('path');
 
 const app = express();
 
-app.use(compression());
-app.use(CONTEXT, express.static(__dirname + '/dist'));
-app.use('/', express.static(__dirname + '/dist'));
-app.listen(PORT, () => console.log(`App running on localhost:${PORT}/${CONTEXT}`));
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/<name-of-app>'));
+
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
+});
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
