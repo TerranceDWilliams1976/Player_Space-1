c0e6f926f0bfbea506ac6d75a010266f57ae4ba6
Justin Davis <justintdavis8@gmail.com>
2018-11-26 08:53:52 -0600

    Updated app.

---
+++ src/app/static/had/had.component.spec.ts
@@ -0,0 +1,24 @@
+import { async, ComponentFixture, TestBed } from "@angular/core/testing";
+
+import { HadComponent } from "./had.component";
+
+describe("HadComponent", () => {
+  let component: HadComponent;
+  let fixture: ComponentFixture<HadComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [HadComponent]
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(HadComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it("should create", () => {
+    expect(component).toBeTruthy();
+  });
+});
